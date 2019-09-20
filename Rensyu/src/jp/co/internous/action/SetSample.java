63b108cf1638808a6c11711693b0f47632827b58
osushitbr <wakamemoe@yahoo.co.jp>
2019-09-20 14:55:15 +0900

    9/202

---
+++ Rensyu/src/jp/co/internous/action/SetSample.java
@@ -0,0 +1,23 @@
+package jp.co.internous.action;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetSample {
+
+	public static void main(String[] args){
+	Set<String>set=new HashSet<String>();
+
+	set.sdd("value1");
+	set.sdd("value2");
+	set.sdd("value3");
+	set.sdd("value4");
+	set.sdd("value5");
+
+	set.add("valie2");
+
+
+	for
+
+	}
+}
